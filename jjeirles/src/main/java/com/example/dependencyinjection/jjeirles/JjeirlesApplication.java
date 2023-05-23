f57790b9e00998c667806ca9b6ed60ad3dd39220
Wisserman <49100018+Wisserman@users.noreply.github.com>
2023-05-23 18:25:19 -0500

    initial commit

---
+++ jjeirles/src/main/java/com/example/dependencyinjection/jjeirles/JjeirlesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinjection.jjeirles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjeirlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjeirlesApplication.class, args);
+	}
+
+}
---
+++ jjeirles/src/test/java/com/example/dependencyinjection/jjeirles/JjeirlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinjection.jjeirles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjeirlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
